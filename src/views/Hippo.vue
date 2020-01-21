c53a6131c8f8591e995bf59557798d00873775bf
Colin Brayton <gringolalia@gmail.com>
2020-01-21 12:32:54 -0300

    rocket to Mars

--- src/views/Hippo.vue
+++ src/views/Hippo.vue
@@ -16,7 +16,7 @@
       h3 The River Horse
       button.uk-button.uk-button-default.uk-button-large
         i.fas.fa-sun.fa-2x.uk-margin-small-right
-        | Imprint Me Me
+        | Imprint Me
 
     .uk-card.uk-card-secondary.uk-card-body.uk-hover.uk-margin-large-bottom
       h1
@@ -24,7 +24,7 @@
       h3 Saint Augustine of Hippo
       button.uk-button.uk-background-primary.uk-button-large
         i.material-icons.md-36.uk-margin-small-right all_inclusive
-        | Oppress Me
+        | Press-Gang Me
 
     .uk-card.uk-card-default.uk-card-body.uk-hover.uk-margin-large-bottom.uk-margin-medium-right
       h1
@@ -32,10 +32,10 @@
       h3 A Faulty Hippocampus
       button.uk-button.uk-button-danger.uk-button-large
         i.material-icons.md-24.uk-margin-small-right toys
-        | Oppress Me
+        | Free Press Me
 
 
-    .uk-card.uk-card-default.uk-card-body.uk-hover.uk-margin-large-bottom.uk-margin-medium-right
+    .uk-card.uk-card-primary.uk-card-body.uk-hover.uk-margin-large-bottom.uk-margin-medium-right
       h1
         i.material-icons.md-72 toys
       h3 The Hippodromes 
@@ -55,28 +55,28 @@
   
     div(class="uk-child-width-1-3@m" uk-grid)
       div
-        .uk-card.uk-card-default.uk-card-body.uk-card-hover
+        .uk-card.uk-card-default.uk-card-small.uk-card-body.uk-card-hover
           .uk-card-media-top
-            img(src='../assets/images/morelinot.jpg' alt)
+            img(src='../assets/images/ponteavmarinho.jpg' alt style="filter: grayscale(95%)")
           .uk-card-body
-            h3.uk-card-title Infernal Machine Seven
+            h3.uk-card-title San Pavlov Skyline
             p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
             button.uk-button.uk-button-primary.uk-button-medium.uk-align-center More Info
       div
-        .uk-card.uk-card-default.uk-card-body.uk-card-hover
+        .uk-card.uk-card-default.uk-card-small.uk-card-body.uk-card-hover
           .uk-card-body
-            h3.uk-card-title Infernal Machine Eight
+            h3.uk-card-title San Pavlov Skyline
             p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
             button.uk-button.uk-button-primary.uk-button-medium.uk-align-center More Info
           .uk-card-media-bottom
-            img(src='../assets/images/morelinot.jpg' alt)
+            img(src='../assets/images/ponteavmarinho.jpg' alt style="filter: grayscale(95%)")
 
       div
-        .uk-card.uk-card-default.uk-card-body.uk-card-hover
+        .uk-card.uk-card-default.uk-card-small.uk-card-body.uk-card-hover
           .uk-card-media-top
-            img(src='../assets/images/morelinot.jpg' alt)
+            img(src='../assets/images/ponteavmarinho.jpg' alt style="filter: grayscale(95%)")
           .uk-card-body
-            h3.uk-card-title Infernal Machine Nine
+            h3.uk-card-title San Pavlov Skyline
             p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
             button.uk-button.uk-button-primary.uk-button-medium.uk-align-center More Info
 

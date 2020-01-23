9f23b209cfd0e70fe358395001eae0f35a354aef
Colin Brayton <gringolalia@gmail.com>
2020-01-23 16:41:36 -0300

    uikit in root of project

--- src/views/Giraffe.vue
+++ src/views/Giraffe.vue
@@ -4,16 +4,16 @@
     .uk-card.uk-card-primary.uk-card-body.uk-hover.uk-margin-large-bottom.uk-margin-medium-right
       h1
         i.material-icons.md-72 toys
-      h3 Addio Mondo Crudele
+      h3 Gaffes by Giraffes
       a.uk-button.uk-button-default.uk-button-large(href="#downward")
         i.fas.fa-bullseye.fa-2x.uk-margin-small-right
-        | Oppress Me
+        | Suppress Me
 
 
     .uk-card.uk-card-default.uk-card-body.uk-hover.uk-margin-large-bottom.uk-margin-medium-right
       h1
         i.material-icons.md-72 directions_bike
-      h3 Addio Mondo Crudele
+      h3 Mimeograph for Staff
       button.uk-button.uk-button-default.uk-button-large
         i.fas.fa-sun.fa-2x.uk-margin-small-right
         | Oppress Me
@@ -21,7 +21,7 @@
     .uk-card.uk-card-secondary.uk-card-body.uk-hover.uk-margin-large-bottom
       h1
         i.material-icons.md-72 beach_access
-      h3 Addio Mondo Crudele
+      h3 Songs by Piaf
       button.uk-button.uk-background-primary.uk-button-large
         i.material-icons.md-36.uk-margin-small-right all_inclusive
         | Oppress Me
@@ -29,16 +29,16 @@
     .uk-card.uk-card-default.uk-card-body.uk-hover.uk-margin-large-bottom.uk-margin-medium-right
       h1
          i.material-icons.md-72 code
-      h3 Addio Mondo Crudele
+      h3 A Graph on Behalf
       button.uk-button.uk-button-danger.uk-button-large
         i.material-icons.md-24.uk-margin-small-right toys
         | Oppress Me
 
 
-    .uk-card.uk-card-default.uk-card-body.uk-hover.uk-margin-large-bottom.uk-margin-medium-right
+    .uk-card.uk-card-primary.uk-card-body.uk-hover.uk-margin-large-bottom.uk-margin-medium-right
       h1
         i.material-icons.md-72 toys
-      h3 Addio Mondo Crudele
+      h3 Telegraph Paragraph
       button.uk-button.uk-button-default.uk-button-large
         i.fas.fa-sun.fa-2x.uk-margin-small-right
         | Oppress Me
@@ -46,7 +46,7 @@
     .uk-card.uk-card-default.uk-card-body.uk-hover.uk-margin-large-bottom
       h1
         i.material-icons.md-72 my_location
-      h3 Addio Mondo Crudele
+      h3 The Golden Calf
       button.uk-button.uk-button-primary.uk-button-large
         i.material-icons.md-36.uk-margin-small-right all_inclusive
         | Oppress Me
@@ -59,13 +59,13 @@
           .uk-card-media-top
             img(src='../assets/images/morelinot.jpg' alt)
           .uk-card-body
-            h3.uk-card-title Infernal Machine Seven
+            h3.uk-card-title Satanic Mills
             p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
             button.uk-button.uk-button-primary.uk-button-medium.uk-align-center More Info
       div
         .uk-card.uk-card-default.uk-card-body.uk-card-hover
           .uk-card-body
-            h3.uk-card-title Infernal Machine Eight
+            h3.uk-card-title Gizmos au Gogo
             p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
             button.uk-button.uk-button-primary.uk-button-medium.uk-align-center More Info
           .uk-card-media-bottom
@@ -76,7 +76,7 @@
           .uk-card-media-top
             img(src='../assets/images/morelinot.jpg' alt)
           .uk-card-body
-            h3.uk-card-title Infernal Machine Nine
+            h3.uk-card-title A Huge Kludge
             p Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.
             button.uk-button.uk-button-primary.uk-button-medium.uk-align-center More Info
 
